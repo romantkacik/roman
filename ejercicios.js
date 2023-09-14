988a4ec399d75e091b71e97445e08aa32939ab32
romantkacik <romantkacik@gmail.com>
2023-09-14 20:39:27 +0200

    ejercicio 11

--- ejercicios.js
+++ ejercicios.js
@@ -72,7 +72,83 @@
 
 //Elimina el segundo elemento del array y muestra el array por consola.
 
-const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+// const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
 
-rickAndMortyCharacters.splice(1, 1);
-console.log(rickAndMortyCharacters);
+// rickAndMortyCharacters.splice(1, 1);
+// console.log(rickAndMortyCharacters);
+
+
+
+
+
+// const number1 = 10; const number2 = 20; const number3 = 2;
+
+
+// ejemplo if(number1 === 10){
+// console.log('number1 es estrictamente igual a 10') }
+
+
+// if (number2 / number1 === 2) { console.log("number2 dividido entre number1 es igual a 2");
+// }
+// if (number1 !== number2) { console.log("number1 es estrictamente distinto a number2");
+// }
+// if (number3 !== number1) { console.log("number3 es distinto number1");
+// }
+// if (number3 * 5 === number1) { console.log("number3 por 5 es igual a number1");
+// }
+// if (number3 * 5 === number1 && number1 * 2 === number2) { console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+// }
+// if (2 === number1 || number1 / 5 === number3) { console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+// }
+
+
+
+
+// Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+
+// for (let i=0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 === 0) {
+//       console.log(i);
+//     }
+//   }
+
+//   for (let i = 1; i <= 10; i++) {
+//     if (i < 10) {
+//       console.log(`Intentando dormir ovejas ${i}`);
+//     } else {
+//       console.log("Dormido!");
+//     }
+//   }
+
+
+// let numberOne = 2
+// let numberTwo = 4
+
+// function getMaxNumber(numberOne, numberTwo) {
+//   if (numberOne > numberTwo) {
+//     return numberOne;
+//   } else {
+//     return numberTwo;
+//   }
+  
+// }
+
+// console.log(getMaxNumber(numberOne, numberTwo));
+
+let numberOne = 9
+let numberTwo = 5
+
+function getMaxNumber(numberOne, numberTwo) {
+  if (numberOne > numberTwo) {
+    return numberOne;
+  } else {
+    return numberTwo;
+  }
+  
+}
+
+console.log(getMaxNumber(numberOne, numberTwo));
